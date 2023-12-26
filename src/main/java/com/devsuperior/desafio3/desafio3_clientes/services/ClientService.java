255d54ebc7e8bbd0e5d66977f383a438d72fbc3b
Achette <i.achette@live.com>
2023-12-26 19:15:03 -0300

    feat: adiciona controller e service para findById

--- src/main/java/com/devsuperior/desafio3/desafio3_clientes/services/ClientService.java
+++ src/main/java/com/devsuperior/desafio3/desafio3_clientes/services/ClientService.java
@@ -1,2 +1,20 @@
-package com.devsuperior.desafio3.desafio3_clientes.services;public class ClientService {
+package com.devsuperior.desafio3.desafio3_clientes.services;
+
+import com.devsuperior.desafio3.desafio3_clientes.dto.ClientDTO;
+import com.devsuperior.desafio3.desafio3_clientes.entity.Client;
+import com.devsuperior.desafio3.desafio3_clientes.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
 }
