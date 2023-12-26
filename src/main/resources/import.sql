fbdfe766e2193fd7a33a8c6e2bd3cfc84a8674de
Achette <i.achette@live.com>
2023-12-26 18:42:24 -0300

    feat: cria entidade client, tabela no banco de dados e valores para o bd

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Maria Brown', '097.985.236-01', 5000.0, TIMESTAMP WITH TIME ZONE '1994-07-20', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Isabella Smith', '123.456.789-01', 5500.0, TIMESTAMP WITH TIME ZONE '1990-05-15', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Michael Johnson', '234.567.890-12', 8000.0, TIMESTAMP WITH TIME ZONE '1988-02-28', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sophia Davis', '345.678.901-23', 4800.0, TIMESTAMP WITH TIME ZONE '2000-10-05', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ethan Miller', '456.789.012-34', 6000.0, TIMESTAMP WITH TIME ZONE '1992-12-18', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Olivia Taylor', '567.890.123-45', 4000.0, TIMESTAMP WITH TIME ZONE '1985-09-10', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('William Rodriguez', '678.901.234-56', 7000.0, TIMESTAMP WITH TIME ZONE '1975-04-25', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ava Turner', '789.012.345-67', 6200.0, TIMESTAMP WITH TIME ZONE '1995-08-15', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Daniel Adams', '890.123.456-78', 7500.0, TIMESTAMP WITH TIME ZONE '1980-03-20', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Emma Baker', '901.234.567-89', 5300.0, TIMESTAMP WITH TIME ZONE '1998-06-30', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Liam Mitchell', '098.765.432-10', 5800.0, TIMESTAMP WITH TIME ZONE '1987-11-12', 1);
+
+
+
+
+
