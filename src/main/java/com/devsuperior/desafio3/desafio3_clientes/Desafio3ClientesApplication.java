919b813b762694c111b9b3ec3ebe213c5e57da24
Achette <i.achette@live.com>
2023-12-26 18:16:51 -0300

    feat: inicia projeto, instala dependencias e adiciona configurações

---
+++ src/main/java/com/devsuperior/desafio3/desafio3_clientes/Desafio3ClientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio3.desafio3_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio3ClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio3ClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafio3/desafio3_clientes/Desafio3ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio3.desafio3_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio3ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
